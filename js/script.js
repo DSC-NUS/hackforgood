6cb82f0e6b1e38ab3504ec8185ace7d21b281787
Head of Design <design.head.dscnus@gmail.com>
2020-02-19 22:07:44 +0800

    added more information

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ function topFunction() {
 // --- TIMER ---
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("March 17, 2020 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
