57304505d2409a7b3dd80527659d30f36a57b641
aliciatxl <example@mail.com>
2020-02-25 22:59:10 +0800

    Change server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 app.listen(port, () => {
     console.log("server is up")
