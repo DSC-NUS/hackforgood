189781a7157c23d09b26483671c3ab6bbbb46f9d
aliciatxl <example@mail.com>
2020-03-14 11:18:22 +0800

    Change submission date

--- public/js/script.js
+++ public/js/script.js
@@ -56,7 +56,7 @@ function topFunction() {
 // --- TIMER ---
 
 // Set the date we're counting down to
-var countDownDate = new Date("March 17, 2020 12:00:00").getTime();
+var countDownDate = new Date("March 19, 2020 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
